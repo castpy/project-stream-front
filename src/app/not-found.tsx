fbb7d6bb53253bc4c8b98828656aaa91b6451230
Marcus Castilho <marcus.castilho@govone.digital>
2024-06-10 11:01:39 -0300

    feat: app bar modelo

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFoundTemplate from '@/templates/NotFound'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <NotFoundTemplate />
+  )
+}
+
+export default NotFoundPage
